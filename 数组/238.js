3e23a14dd0292cc2c24b926d8b46ecce3ca03ec4
zwh <zhangweihan008@qq.com>
2025-01-14 18:35:56 +0800

    feat: 238题，辅助列表

--- 数组/238.js
+++ 数组/238.js
@@ -3,5 +3,41 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    
+    const left = []
+    const right = []
+    const len = nums.length
+
+    left[0] = 1
+    for (var i = 1; i < len; i++) {
+      left[i] = left[i - 1] * nums[i - 1]
+    }
+
+    right[len - 1] = 1
+    for(let i = len - 2; i >= 0; i--){
+      right[i] = right[i + 1] * nums[i + 1]
+    }
+
+    const res = []
+    for(let i = 0; i < len; i++) {
+      res[i] = left[i] * right[i]
+    }
+
+    return res
 };
+
+function test () {
+  const nums = [1,2,3,4]
+  const res = productExceptSelf(nums)
+
+  console.log('res ->', res);
+}
+test()
+
+
+function test1 () {
+  const nums = [-1,1,0,-3,3]
+  const res = productExceptSelf(nums)
+  
+  console.log('res ->', res);
+}
+test1()
